9c2a44b51fb1eacc9a87da89c82f583b901bbe0a
DESKTOP-D351Q1F\Bartek <bac2@vp.pl>
2019-03-27 17:18:54 +0100

    added bootstrap/jquery packages, added main routing, added authentication module

--- KOService.Presentation/KOService/src/app/app-routing.module.ts
+++ KOService.Presentation/KOService/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', children: [
+    { path: 'login', loadChildren: './authentication/authentication.module#AuthenticationModule' },
+    { path: 'admin', loadChildren: './admin/admin.module#AdminModule' },
+    { path: 'manager', loadChildren: './manager/manager.module#ManagerModule' },
+    { path: 'mechanic', loadChildren: './mechanic/mechanic.module#MechanicModule' },
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ KOService.Presentation/KOService/src/app/authentication/authentication.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { AuthenticationRoutingModule } from './authentication-routing.module'
+
+@NgModule({
+  imports: [
+    CommonModule,
+    AuthenticationRoutingModule
+  ],
+  declarations: [LoginComponent]
+})
+export class AuthenticationModule { }
