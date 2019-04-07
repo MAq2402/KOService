d51d7b62b2ac561cba3e23373697bd4f0ef1987c
Tomasz Kus <tomasz.kus97@gmail.com>
2019-04-07 19:29:19 +0200

    activity entity + service

--- KOService.Application/Migrations/20190403132558_IdentityRole.Designer.cs
+++ KOService.Application/Migrations/20190403132558_IdentityRole.Designer.cs
@@ -4,14 +4,16 @@ using KOService.Domain.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace KOService.Domain.Migrations
 {
     [DbContext(typeof(KOServiceDbContext))]
-    partial class KOServiceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190403132558_IdentityRole")]
+    partial class IdentityRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
