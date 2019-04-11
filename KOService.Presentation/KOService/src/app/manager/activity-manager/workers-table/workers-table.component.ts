df139fd5b1667401a648692df2ca8d11c6650ffa
Tomasz Kus <tomasz.kus97@gmail.com>
2019-04-11 12:39:16 +0200

    added map in wrokers-table, con fill it with data from service

--- KOService.Presentation/KOService/src/app/manager/activity-manager/workers-table/workers-table.component.ts
+++ KOService.Presentation/KOService/src/app/manager/activity-manager/workers-table/workers-table.component.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Employee } from 'src/app/shared/models/Employee';
 import { EmployeeService } from 'src/app/shared/services/employee.service';
 import { Role } from 'src/app/shared/enums/Role';
+import { Activity } from 'src/app/shared/models/Activity';
+import { ActivityService } from 'src/app/shared/services/activity.service';
+
+export interface WorkersActivities {
+  [workerId: string]: Activity[];
+}
 
 @Component({
   selector: 'app-workers-table',
@@ -11,13 +17,29 @@ import { Role } from 'src/app/shared/enums/Role';
 export class WorkersTableComponent implements OnInit {
 
   workers: Employee[];
-  columnsToDisplay = ['firstName', 'lastName'];
-  constructor(private employeeService: EmployeeService) { }
+  workersActivities: Map<string,Activity[]>;
+  // Just for now to test nested request to find out if it works
+  moqWorkerId = '1';
+  columnsToDisplay = ['firstName', 'lastName', 'numberOfActivities'];
+  constructor(private employeeService: EmployeeService, private activityService: ActivityService) {
+    this.workersActivities = new Map<string,Activity[]>();
+   }
 
   ngOnInit() {
     this.employeeService.getEmployeesByRole(Role.mechanic).subscribe(
-      mechanics => this.workers = mechanics
+      mechanics =>(this.workers = mechanics,mechanics.map(
+
+        //mechanic => this.activityService.getWorkerActivities(mechanic.id)
+        mechanic => (this.activityService.getWorkerActivities(this.moqWorkerId).subscribe(
+          mechanicActivities => (
+           this.workersActivities[mechanic.id] = mechanicActivities,
+            console.log(this.workersActivities[mechanic.id])
+            )
+            )
+        )))
   );
+  
+  
   }
 
 }
