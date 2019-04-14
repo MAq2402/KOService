b9fb2e10f441139d25d50e33940e3a733e417b8d
Kuba <kubeckk21@gmail.com>
2019-04-14 22:35:49 +0200

    removed errors from form, dropdown works

--- KOService.Presentation/KOService/src/app/admin/add-employee-form/add-employee-form.component.ts
+++ KOService.Presentation/KOService/src/app/admin/add-employee-form/add-employee-form.component.ts
@@ -1,19 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
 import { Role } from '../../shared/enums/Role';
-import { forEach } from '@angular/router/src/utils/collection';
 
 @Component({
   selector: 'app-add-employee-form',
   templateUrl: './add-employee-form.component.html',
   styleUrls: ['./add-employee-form.component.css']
 })
-export class AddEmployeeFormComponent implements OnInit {  
+export class AddEmployeeFormComponent {  
   roles = Role;
-  constructor() { }
+  keys: any [];
 
-  ngOnInit() {
-    console.log(this.roles);
-  }
+  constructor() {
+    this.keys = Object.keys(Role).filter(k => !isNaN(Number(k)));
+   }
 
 }
