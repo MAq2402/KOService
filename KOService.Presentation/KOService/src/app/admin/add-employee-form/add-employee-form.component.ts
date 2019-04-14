87d164b20fc1f7768d38415f7792e1c5ba21599d
Kuba <kubeckk21@gmail.com>
2019-04-14 18:32:57 +0200

    developing form, select not working

--- KOService.Presentation/KOService/src/app/admin/add-employee-form/add-employee-form.component.ts
+++ KOService.Presentation/KOService/src/app/admin/add-employee-form/add-employee-form.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Role } from '../../shared/enums/Role';
+import { forEach } from '@angular/router/src/utils/collection';
+
 @Component({
   selector: 'app-add-employee-form',
   templateUrl: './add-employee-form.component.html',
   styleUrls: ['./add-employee-form.component.css']
 })
-export class AddEmployeeFormComponent implements OnInit {
-
+export class AddEmployeeFormComponent implements OnInit {  
+  roles = Role;
   constructor() { }
 
   ngOnInit() {
+    console.log(this.roles);
   }
 
 }
