9a39701df891cd3611821cfab167d3036f928e8b
Jakub Lyko <kubeckk21@gmail.com>
2019-04-29 18:53:11 +0200

    changed design of employee-adding-form, changed classes to used in login form

--- KOService.Presentation/KOService/src/app/admin/add-employee-form/add-employee-form.component.ts
+++ KOService.Presentation/KOService/src/app/admin/add-employee-form/add-employee-form.component.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
+import { Employee } from '../../shared/models/employee.model';
 import { Role } from '../../shared/enums/Role';
 
 @Component({
-  selector: 'app-add-employee-form',
-  templateUrl: './add-employee-form.component.html',
-  styleUrls: ['./add-employee-form.component.css']
+    selector: 'app-add-employee-form',
+    templateUrl: './add-employee-form.component.html',
+    styleUrls: ['./add-employee-form.component.css']
 })
-export class AddEmployeeFormComponent {  
-  roles = Role;
-  keys: any [];
+export class AddEmployeeFormComponent {
+    roles = Role;
+    keys: any[];
 
-  constructor() {
-    this.keys = Object.keys(Role).filter(k => !isNaN(Number(k)));
-   }
+    constructor() {
+        this.keys = Object.keys(Role).filter(k => !isNaN(Number(k)));
+    }
 
 }
