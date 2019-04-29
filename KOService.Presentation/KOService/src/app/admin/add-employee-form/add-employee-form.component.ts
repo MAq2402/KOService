8fc7ef19c731d29ffbc4b5e8b64b0ae2fc38c567
Jakub Lyko <kubeckk21@gmail.com>
2019-04-29 19:03:07 +0200

    positioned form a bit

--- KOService.Presentation/KOService/src/app/admin/add-employee-form/add-employee-form.component.ts
+++ KOService.Presentation/KOService/src/app/admin/add-employee-form/add-employee-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { Employee } from '../../shared/models/employee.model';
 import { Role } from '../../shared/enums/Role';
 
@@ -7,12 +7,19 @@ import { Role } from '../../shared/enums/Role';
     templateUrl: './add-employee-form.component.html',
     styleUrls: ['./add-employee-form.component.css']
 })
-export class AddEmployeeFormComponent {
+export class AddEmployeeFormComponent implements OnInit {
     roles = Role;
     keys: any[];
 
+    @ViewChild('firstName') firstName: ElementRef;
+    @ViewChild('lastName') lastName: ElementRef;
+
     constructor() {
         this.keys = Object.keys(Role).filter(k => !isNaN(Number(k)));
+    }
+
+    ngOnInit() {
+
     }
 
 }
