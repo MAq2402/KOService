8b9bb6bb3a4a2a0c79b19e3617af5a8ea13f310f
tomaszkus97 <tomi88d@interia.pl>
2019-05-03 23:34:48 +0200

    integration test configuration

---
+++ KOService.Tests/Controllers/ActivityControllerIntegrationTests.cs
@@ -0,0 +1,41 @@
+﻿using KOService.Domain.Entities;
+using KOService.WebAPI;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace KOService.Tests.Controllers
+{
+    class ActivityControllerIntegrationTests : IClassFixture<InMemoryWebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public ActivityControllerIntegrationTests(InMemoryWebApplicationFactory<Startup> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task given_existing_mechanicId_returns_activities()
+        {
+            var httpResponse = _client.GetAsync("/api/activities/mechanic/1");
+
+            Assert.IsType<OkObjectResult>(httpResponse.Status);
+
+            var result = httpResponse.Result;
+            var stringResult = await result.Content.ReadAsStringAsync();
+
+            var players = JsonConvert.DeserializeObject<IEnumerable<Activity>>(stringResult);
+            Assert.Contains(players, p => p.FirstName == "Wayne");
+            Assert.Contains(players, p => p.FirstName == "Mario");
+
+        }
+
+
+    }
+}
---
+++ KOService.Tests/SeedData.cs
@@ -0,0 +1,17 @@
+﻿using KOService.Domain.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KOService.Tests
+{
+    public static class SeedData
+    {
+        public static void PopulateTestData(KOServiceDbContext dbContext)
+        {
+            //Tu mozna dodac wlasne dane testowe
+            dbContext.Activities.Add(new Domain.Entities.Activity(Guid.NewGuid(),Guid.NewGuid(), "wymiana silnika", 0));
+            dbContext.SaveChanges();
+        }
+    }
+}
