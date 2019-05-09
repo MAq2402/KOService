9dc5355a2db17b9088c73f683bfaeff8191f02de
Tomasz Kus <tomasz.kus97@gmail.com>
2019-05-09 13:12:16 +0200

    couple fixes and workerswithactivities query

--- KOService.Application/Queries/Activity/GetWorkersWithActivitiesQuery.cs
+++ KOService.Application/Queries/Activity/GetWorkersWithActivitiesQuery.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿using KOService.Application.DTOs.Activity;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace KOService.Application.Queries.Activity
 {
-    class GetWorkersWithActivitiesQuery
+    public class GetWorkersWithActivitiesQuery : IRequest<IEnumerable<WorkersWithActivitiesDto>>
     {
     }
 }
