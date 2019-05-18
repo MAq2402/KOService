d05b544da06724dc6841f08c0865ed68f0513c8b
martyna8821 <martyna.drabinska@gmail.com>
2019-05-18 18:51:49 +0200

    simple fix v2

--- KOService.Presentation/KOService/src/app/app.module.ts
+++ KOService.Presentation/KOService/src/app/app.module.ts
@@ -1,24 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { SharedModule } from './shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ManagerModule } from './manager/manager.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
     CoreModule.forRoot(),
-    SharedModule,
     BrowserAnimationsModule,
-    ManagerModule
+    AppRoutingModule
   ],
   exports: [],
   providers: [],
